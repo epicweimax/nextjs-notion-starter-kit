db24aef1e09ef645e1496c1f24bdf880ceb8f62c
epicweimax <80211027+epicweimax@users.noreply.github.com>
2021-12-30 23:07:38 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'cf8766aa890a436ba227d769d8ba08e5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '杂乱无章',
+  domain: 'cf8766aa890a436ba227d769d8ba08e5',
+  author: 'EpicMax',
 
   // open graph metadata (optional)
   description: 'Example site description',
